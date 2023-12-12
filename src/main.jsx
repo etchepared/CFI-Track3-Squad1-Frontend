6be14d9b56f3c56520c0d29443517e2a148d451c
etchepared <88214684+etchepared@users.noreply.github.com>
2023-12-12 16:21:33 -0300

    fix router structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AppRouter from "./AppRouter.jsx";
 import "./index.css";
-
-const router = createBrowserRouter([
-  /*Para crear rutas una vez generado el .jsx dupliquen la estructura indicando el path y realicen la importación correspondiente
-  {
-    path: "/ejemplo",
-    element: <Ejemplo />,
-  },
-  */
-  {
-    path: "/",
-    element: <App />,
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <AppRouter />
   </React.StrictMode>,
 );
